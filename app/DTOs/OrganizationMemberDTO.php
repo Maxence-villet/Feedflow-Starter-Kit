89f0568b9d0e7dd1c76b2bdba27874d965957c4c
AmandineGUISY <aguisy@yahoo.fr>
2025-12-17 11:28:52 +0100

    Making action and dto for the members organization actions

--- app/DTOs/OrganizationMemberDTO.php
+++ app/DTOs/OrganizationMemberDTO.php
@@ -3,15 +3,22 @@
 namespace App\DTOs;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\OrganizationUser\StoreOrganizationUser;
 
 final class OrganizationMemberDTO
 {
     private function __construct(
+        public readonly int $user_id,
+        public readonly string $role,
+        public readonly int $organization_id,
     ) {}
 
-    public static function fromRequest(Request $request): self
+    public static function fromRequest(StoreOrganizationUser $request): self
     {
         return new self(
+            user_id: $request->user_id,
+            role: $request->role,
+            organization_id: $request->route('organization')->id
         );
     }
 }
