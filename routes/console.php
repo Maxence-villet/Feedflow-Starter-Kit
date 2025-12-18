5f82ef80983115bf8069cb64f982c3aa4740c5a5
AmandineGUISY <aguisy@yahoo.fr>
2025-12-18 11:54:53 +0100

    Fixing the schredule

--- routes/console.php
+++ routes/console.php
@@ -2,13 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::call(function (Schedule $schedule) {
-    $schedule->command('app:send-survey-daily-reports')->dailyAt('08:00');
-});
+Schedule::command('app:send-survey-daily-reports')->dailyAt('08:00');
 
