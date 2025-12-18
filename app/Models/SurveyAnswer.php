e7b81b893a49c545dce6c1d6674fa810f5485ea9
Nicolas-77950 <nicolascheel77@gmail.com>
2025-12-18 11:55:23 +0100

    adding sql requete

--- app/Models/SurveyAnswer.php
+++ app/Models/SurveyAnswer.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon; 
 
 class SurveyAnswer extends Model
 {
@@ -11,11 +13,24 @@ class SurveyAnswer extends Model
 
     protected $table    = 'survey_answers';
     public $timestamps  = true;
+    
     protected $fillable = [
         'id', 'survey_id', 'survey_question_id', 'user_id',
         'answer',
         'created_at', 'updated_at'
     ];
+
     protected $casts = [
     ];
+
+    public static function getData() {
+        return self::select(
+                DB::raw('DATE(created_at) as date'),
+                DB::raw('count(*) as total')
+            )
+            ->where('created_at', '>=', Carbon::now()->startOfWeek())
+            ->groupBy('date')
+            ->orderBy('date', 'ASC')
+            ->get();
+    }
 }
