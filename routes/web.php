52dcd2f96a087850475ed6e379c300811b3ea99b
Nicolas-77950 <nicolascheel77@gmail.com>
2025-12-18 11:55:55 +0100

    update route for dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,15 @@ use App\Http\Controllers\OrganizationController;
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ChartController;
+
+Route::get('/dashboard', [ChartController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
