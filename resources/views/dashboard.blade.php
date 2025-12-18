b144f53d7829ef30bed9679bd892974d3ce4e2a4
Nicolas-77950 <nicolascheel77@gmail.com>
2025-12-18 11:57:15 +0100

    add link js for dahsbord-chartjs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,20 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h3 class="text-lg font-semibold mb-4">Sondages confirmés par jour</h3>
+                    
+                    <div style="position: relative; height: 400px; width: 100%;">
+                        <canvas id="myChart" 
+                            data-labels="{{ json_encode($chartLabels) }}" 
+                            data-values="{{ json_encode($chartData) }}">
+                        </canvas>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    
+    <script src="{{ asset('js/dashboard-charts.js') }}?v={{ time() }}"></script>
 </x-app-layout>
